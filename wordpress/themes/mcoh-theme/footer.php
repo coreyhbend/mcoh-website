f96d78d5d4e7636e79f2ded8311500e8a70dd7f3
coreyhbend <coreyh.bend@gmail.com>
2026-05-08 14:11:15 -0700

    Add theme footer template

---
+++ wordpress/themes/mcoh-theme/footer.php
@@ -0,0 +1,35 @@
+<footer class="site-footer">
+  <div class="footer-grid">
+    <div>
+      <h3>Make Central Oregon Home</h3>
+      <p>Central Oregon lifestyle, destination, and community resource platform.</p>
+    </div>
+
+    <div>
+      <h4>Explore</h4>
+      <ul>
+        <li><a href="/explore">Things To Do</a></li>
+        <li><a href="/destinations">Destinations</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h4>Communities</h4>
+      <ul>
+        <li><a href="/communities">Community Guides</a></li>
+        <li><a href="/moving-to-bend">Moving to Bend</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h4>Real Estate</h4>
+      <ul>
+        <li><a href="https://centralorproperties.com">Search Homes</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
